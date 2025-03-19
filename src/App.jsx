dea28cd4a64d4276a393a245691a15750d285266
Guido Perman <guidoyaelperman@gmail.com>
2025-03-19 19:04:25 -0300

    add state management for investment details and update UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from './components/Header'
 import { UserInput } from './components/UserInput'
 import { ResultsList } from './components/ResultsList'
 
 function App() {
+  const [investmentDetails, setInvestmentDetails] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0
+  })
+
   return (
     <div>
       <Header></Header>
 
-      <UserInput></UserInput>
+      <UserInput
+        setInvestmentDetails={setInvestmentDetails}
+      ></UserInput>
 
       <ResultsList></ResultsList>
     </div>
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 
-export const UserInput = () => {
+export const UserInput = ({ setInvestmentDetails }) => {
+
+  function onInvestmentDetailsChange(event) {
+    setInvestmentDetails(prevData => {
+      return {
+        ...prevData,
+        [event.target.id]: event.target.value
+      }
+    });
+  }
+  
   return <div id="user-input">
     <div className="input-group">
       <p>
         <label htmlFor="initial">Initial Investment</label>
-        <input id="initial" type="number" />
+        <input id="initialInvestment" type="number" onChange={onInvestmentDetailsChange} />
       </p>
       <p>
         <label htmlFor="anual">Annual Investment</label>
-        <input id="annual" type="number" />
+        <input id="annualInvestment" type="number" onChange={onInvestmentDetailsChange} />
       </p>
 
     </div>
 
     <div className="input-group">
       <p>
-        <label htmlFor="initial">Initial Investment</label>
-        <input id="initial" type="number" />
+        <label htmlFor="initial">Expected Return</label>
+        <input id="expectedReturn" type="number" onChange={onInvestmentDetailsChange} />
       </p>
       <p>
-        <label htmlFor="anual">Annual Investment</label>
-        <input id="annual" type="number" />
+        <label htmlFor="anual">Duration</label>
+        <input id="duration" type="number" onChange={onInvestmentDetailsChange} />
       </p>
     </div>
   </div>
